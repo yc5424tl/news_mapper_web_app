3a721f7e7fc9c2e821df4c5377f3c7d7908e00cb
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-10-03 00:48:24 -0500

    Django-Heroku and Conf Vars Added

--- news_mapper/settings.py
+++ news_mapper/settings.py
@@ -167,10 +167,10 @@ USE_TZ = True
 DATABASES['default'].update(dj_database_url.config())
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+# GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+# GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'news-mapper-app.herokuapp.com']
