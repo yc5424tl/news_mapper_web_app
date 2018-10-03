29280ddd7c33897a0e11177d1452e0e78ca184f5
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-10-02 23:50:05 -0500

    Django-Heroku and Conf Vars Added

--- news_mapper/settings.py
+++ news_mapper/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 
 DATABASE_URL = os.environ.get('MONGODB_URI')
 
-DATABASES['default'].update(dj_database_url.config(default=config('DATABASE_URL')))
+DATABASES['default'].update(dj_database_url.config(default=os.getenv('DATABASE_URL')))
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
