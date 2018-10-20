2d1ee32b211b1296d8ae2d7f39b8317bf2576e04
yc5424tl <17325414+yc5424tl@users.noreply.github.com>
2018-10-20 18:30:16 -0500

    Update release.sh

--- news_mapper_django/release.sh
+++ news_mapper_django/release.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+apt update
+apt upgrade
+apt install gdal-bin libgdal-dev
+
 pip install conda
 conda install gdal fiona rtree shapely pyproj
 python -m pip install -r requirements.txt
