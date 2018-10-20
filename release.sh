d2f05ab7e9296961165e05f0a94ec1fe84ff70e6
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-10-20 15:58:18 -0500

    !

--- release.sh
+++ release.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+pip install conda
+conda install gdal fiona rtree shapely pyproj
+python -m pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
